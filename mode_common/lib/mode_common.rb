46d8f385981624192f9fe5771d47ddf3ce068887
mikepieper <mpieper636@gmail.com>
2024-02-16 23:01:35 -0500

    ignore .DS_Store

---
+++ mode_common/lib/mode_common.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "mode_common/version"
+
+module ModeCommon
+  class Error < StandardError; end
+  # Your code goes here...
+  class Hola
+    def self.hi
+      puts "Hi!"
+    end
+  end
+end
---
+++ mode_common/lib/mode_common/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ModeCommon
+  VERSION = "0.1.0"
+end
